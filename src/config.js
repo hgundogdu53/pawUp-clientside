9533d01c9ab69398c7c84e66e92b9a85b326ce13
Huseyin Gundogdu <hgundogdu53@gmail.com>
2019-11-20 12:50:30 -0800

    deploymnet

--- src/config.js
+++ src/config.js
@@ -1,6 +1,7 @@
 export default {
     API_BASE_URL: process.env.API_BASE_URL,
+    API_KEY: process.env.REACT_APP_API_KEY,
     TOKEN_KEY: process.env.TOKEN_KEY,
-    API_ENDPOINT: 'https://fierce-tor-48814.herokuapp.com/api' || 'http://localhost:8000/api'
+    API_ENDPOINT: `https://fierce-tor-48814.herokuapp.com/api` || `http://localhost:8000/api`
 
 }
